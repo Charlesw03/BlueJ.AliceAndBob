11ea6b95f2542addb2d5441f32eaa6cd88823339
Charles Wilmer <charleswilmer@zipcoders-MacBook-Pro.local>
2019-02-05 19:38:34 -0500

    slowly but surely catching

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+  Scanner reader = new Scanner(System.in);
+  System.out.println("What is your name?");
+   String name = reader.nextLine();
+   
+    if( name.equals("Alice")) {
+        System.out.println("yo Alice");
 
+    } 
+    else if(name.equals("Bob")) {
+        System.out.println( "yo Bob");
+    } else {
+    System.out.println("finish the dam labs");
     }
+  }
+   
 }
